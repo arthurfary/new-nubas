a5e17fcba49b40308379bd4fad809e56e28aec38
arthurfary <arthurfary22@gmail.com>
2023-09-30 00:03:24 -0300

    changed order of functions

--- cogs/currency/currency_cog.py
+++ cogs/currency/currency_cog.py
@@ -21,8 +21,58 @@ class Currency(commands.Cog):
         print('Checando interesse...')
         interest = InterestHandler(self.db_path)
         interest.add_intrest_if_one_day_is_passed()
-    
-    @staticmethod
+        
+    @commands.command()
+    async def join(self, ctx, account_number: int):
+        if self.is_join_valid(ctx, account_number):
+            uid = self.get_uid(ctx)
+            name = self.get_author_name(ctx)
+
+            self.db.create_and_commit_db_entry(uid, account_number, name)
+            await ctx.send(f'Conta criada com o número {account_number}! Bem vindo ao Nubas!')
+
+    @commands.command()
+    async def saldo(self, ctx):
+        if self.is_user_in_db(ctx):
+            await ctx.send(f'Você tem {self.db.fetch_money_by_uid(self.get_uid(ctx)):.2f} na sua conta :moneybag:')
+
+        else:
+            raise Exception("Conta não registrada.")
+
+    @commands.command()
+    async def showdb(self, ctx):
+        if self.is_me(ctx):
+            cursor = self.db.fetch_whole_database()
+            for row in cursor:
+                await ctx.message.author.send(row)
+
+    @commands.command()
+    async def dix(self, ctx, account_number: int, amount: float):
+        if self.is_transfer_valid(ctx, account_number, amount):
+            self.db.transfer_money(self.get_uid(ctx), account_number, amount)
+            await ctx.send(f'{amount} Transferidos para a conta {account_number}')
+
+    @commands.command()
+    async def lst(self, ctx):
+        account_and_names = zip(self.db.fetch_all_accounts(), self.db.fetch_all_names())
+
+        names_in_code = ''
+        for account, name in account_and_names:
+            names_in_code += '```' + f'{account} >>> {name}' + '```'
+
+        await ctx.message.author.send('Pessoas que tem contas: ' + names_in_code)
+
+
+    @commands.command()
+    async def add(self, ctx, account_number: int, amount: float):
+        if self.is_me():
+            if self.is_account_number_in_db(account_number):
+                self.db.add_money_in_account(amount, account_number)
+                await ctx.send(f"{amount} adicionado a conta {account_number}.")
+        
+        else:
+            raise Exception('Conta não existe.')
+
     def is_me(ctx):
         return ctx.message.author.id == 226524214411132928
 
@@ -90,55 +140,4 @@ class Currency(commands.Cog):
             return True
         else:
             return False
-        
-    @commands.command()
-    async def join(self, ctx, account_number: int):
-        if self.is_join_valid(ctx, account_number):
-            uid = self.get_uid(ctx)
-            name = self.get_author_name(ctx)
-
-            self.db.create_and_commit_db_entry(uid, account_number, name)
-            await ctx.send(f'Conta criada com o número {account_number}! Bem vindo ao Nubas!')
-
-    @commands.command()
-    async def saldo(self, ctx):
-        if self.is_user_in_db(ctx):
-            await ctx.send(f'Você tem {self.db.fetch_money_by_uid(self.get_uid(ctx)):.2f} na sua conta :moneybag:')
-
-        else:
-            raise Exception("Conta não registrada.")
-
-    @commands.command()
-    @commands.check(is_me)
-    async def showdb(self, ctx):
-        cursor = self.db.fetch_whole_database()
-        for row in cursor:
-            await ctx.message.author.send(row)
-
-    @commands.command()
-    async def dix(self, ctx, account_number: int, amount: float):
-        if self.is_transfer_valid(ctx, account_number, amount):
-            self.db.transfer_money(self.get_uid(ctx), account_number, amount)
-            await ctx.send(f'{amount} Transferidos para a conta {account_number}')
-
-    @commands.command()
-    async def lst(self, ctx):
-        account_and_names = zip(self.db.fetch_all_accounts(), self.db.fetch_all_names())
-
-        names_in_code = ''
-        for account, name in account_and_names:
-            names_in_code += '```' + f'{account} >>> {name}' + '```'
-
-        await ctx.message.author.send('Pessoas que tem contas: ' + names_in_code)
-
-
-    @commands.command()
-    @commands.check(is_me)
-    async def add(self, ctx, account_number: int, amount: float):
-        if self.is_account_number_in_db(account_number):
-            self.db.add_money_in_account(amount, account_number)
-            await ctx.send(f"{amount} adicionado a conta {account_number}.")
-        
-        else:
-            raise Exception('Conta não existe.')
         
